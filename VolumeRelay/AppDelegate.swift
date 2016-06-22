0537b8f475a4eae5656e992b19c5b84ffec3cfa6
mtin <themtin@gmail.com>
2016-06-23 01:39:52 +0200

    creating xcode project

---
+++ VolumeRelay/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  VolumeRelay
+//
+//  Created by Martin Fritzsche on 23/06/16.
+//  Copyright © 2016 Martin Fritzsche. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
